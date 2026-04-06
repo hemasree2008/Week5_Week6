55a1712ea252593d236351fb7798deca7b1d7dfe
hemasree2008 <hn9341@srmist.edu.in>
2026-04-06 10:24:21 +0530

    Update Program2.java

--- Level1_Programs/Program2.java
+++ Level1_Programs/Program2.java
@@ -1,18 +1,46 @@
+// Program to classify numbers
 import java.util.Scanner;
 
-public class Handshakes {
-
-    public static int calculateHandshakes(int n) {
-        return (n * (n - 1)) / 2;
-    }
-
+class NumberAnalysis {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int students = sc.nextInt();
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Handshakes = " + calculateHandshakes(students));
+        int size = 5;
+        int[] numbers = new int[size];
 
-        sc.close();
+        // Input
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = input.nextInt();
+        }
+
+        // Analysis
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] > 0) {
+                System.out.print(numbers[i] + " is Positive and ");
+
+                if (numbers[i] % 2 == 0)
+                    System.out.println("Even");
+                else
+                    System.out.println("Odd");
+
+            } else if (numbers[i] < 0) {
+                System.out.println(numbers[i] + " is Negative");
+            } else {
+                System.out.println("Number is Zero");
+            }
+        }
+
+        // Compare first and last
+        if (numbers[0] > numbers[size - 1])
+            System.out.println("First element is greater");
+        else if (numbers[0] < numbers[size - 1])
+            System.out.println("Last element is greater");
+        else
+            System.out.println("Both are equal");
+
+        input.close();
     }
 }
